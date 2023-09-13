f998b4c804f7ae7436a23925ccd641b8084d38fd
Eduard Wolf <eduard.wolf@gls-ecl.com>
2023-09-13 13:54:10 +0200

    SESA-2186: Upgrade version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             create<MavenPublication>("addressFormatter") {
                 groupId = "com.bettermile.betterroute"
                 artifactId = "address-formatter-android"
-                version = "0.1.5"
+                version = "0.1.6"
                 from(components["release"])
             }
         }
