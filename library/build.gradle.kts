66d03a898fa16441b80197b99ef2c59c6db4642c
Eduard Wolf <eduard.wolf@gls-ecl.com>
2024-01-31 17:38:27 +0100

    Upgrade version to 0.1.7

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             create<MavenPublication>("addressFormatter") {
                 groupId = "com.bettermile.betterroute"
                 artifactId = "address-formatter-android"
-                version = "0.1.6"
+                version = "0.1.7"
                 from(components["release"])
             }
         }
