9838f744c90d3ede266e02ecd109778b8467de7c
Eduard Wolf <eduard.wolf@gls-ecl.com>
2024-09-27 11:42:41 +0200

    Prepare 0.3.1 release

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -43,7 +43,7 @@ mavenPublishing {
         signAllPublications()
     }
 
-    coordinates("com.bettermile", "address-formatter-kotlin", "0.3.0")
+    coordinates("com.bettermile", "address-formatter-kotlin", "0.3.1")
 
     pom {
         name.set("Address Formatter Kotlin")
